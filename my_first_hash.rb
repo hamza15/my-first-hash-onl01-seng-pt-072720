cef0c0cdd67e55263c6a3ac60d883ca36b32c24b
Sardar Hamza Naqi <sardarhamzanaqi@gmail.com>
2020-08-02 22:33:21 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -14,7 +14,6 @@ def shipping_manifest
   the_manifest["porcelain vases"] = 2 
   the_manifest["oil paintings"] = 3
   the_manifest
-
 end
 
 def retrieval
@@ -25,6 +24,7 @@ def retrieval
                  }
 
   # Look up the value of the "oil paintings" key in the shipping_manifest hash below
+  shipping_manifest["oil paintings"]
 
 end
 
@@ -35,13 +35,13 @@ def adding
                   "oil paintings" => 3
                  }
   # add 2 muskets to the shipping_manifest hash below
-
+  shipping_manifest["muskets"] = 2 
 
 
   # add 4 gun powder to the shipping_manifest hash below
-
+  shipping_manifest["gun powder"] = 4
 
 
   # return the shipping_manifest hash below
-
+  shipping_manifest
 end
