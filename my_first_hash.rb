5a7fa5a147996d890a350549d582575e530c53ed
Sardar Hamza Naqi <sardarhamzanaqi@gmail.com>
2020-08-02 22:22:32 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -12,8 +12,8 @@ def shipping_manifest
   the_manifest = {}
   the_manifest["whale bone corsets"] = 5
   the_manifest["porcelain vases"] = 2 
-  the_manifest[""]
-
+  the_manifest["oil paintings"] = 3
+  the_manifest
 
 end
 
